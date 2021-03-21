7e981f269cba03f0758855cb6549cd87681ff417
sadok jbenyeni <sadok.jbenyeni@protonmail.com>
2021-03-21 08:31:16 +0100

    Deleted the app_server folder

--- app_server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const mainController = require('../controllers/homepage');
-
-router.get('/', mainController.sealsList);
-router.get('/', mainController.sealDetails);
-router.post('/', mainController.createNewSeal);
-module.exports = router;
